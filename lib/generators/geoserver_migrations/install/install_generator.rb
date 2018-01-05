e0a30dc421aea6991971206bde10e3725c36ec30
nathanvda <nathan@dixis.com>
2018-01-05 14:47:51 +0100

    Fix location of generator

---
+++ lib/generators/geoserver_migrations/install/install_generator.rb
@@ -0,0 +1,19 @@
+module GeoserverMigrations
+  module Generators
+    class InstallGenerator < ::Rails::Generators::Base
+      source_root File.expand_path('../templates', __FILE__)
+      desc "This generator installs the default config file, and glue code into application-controller; which you can edit"
+
+
+      def copy_config_file
+        copy_file "geoserver_migrations_config.yml", "config/geoserver_migrations_config.yml"
+      end
+
+      def create_migrations_folder
+        # empty_directory "geoserver", "migrate"
+        empty_directory "geoserver/migrate"
+      end
+
+    end
+  end
+end
