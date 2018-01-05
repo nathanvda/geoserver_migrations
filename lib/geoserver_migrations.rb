4279076880aaa720487fc4f1beee3a8a5fcef137
nathanvda <nathan@dixis.com>
2018-01-05 14:48:00 +0100

    Make it an engine!

--- lib/geoserver_migrations.rb
+++ lib/geoserver_migrations.rb
@@ -1,2 +1,53 @@
 module GeoserverMigrations
+
+  #define exceptions
+  module ExceptionsMixin
+    def initialize(str)
+      super("GeoserverMigrations: " + str)
+    end
+  end
+  class ArgumentError < ::ArgumentError #:nodoc:
+    include ExceptionsMixin
+  end
+
+  # class AccessDenied < ::StandardError #:nodoc:
+  #   include ExceptionsMixin
+  #   def initialize(str='Access denied: you do not have the right permissions for the requested action.')
+  #     super(str)
+  #   end
+  # end
+
+
+  class Engine < ::Rails::Engine
+
+    # configure our plugin on boot
+    initializer "geoserver_migrations.initialize" do |app|
+
+      # mixin our code ?
+      # ActionController::Base.send(:include, Vigilante::ControllerExtension)
+      # ActiveRecord::Base.send(:include, Vigilante::ActiveRecordExtensions)
+
+      #load configuration files if they are available
+      geoserver_config_file         = File.join("#{Rails.root.to_s}", 'config', 'geoserver_migrations_config.yml')
+
+      if File.exist?(geoserver_config_file)
+        Rails.logger.debug "define GEOSERVER_MIGRATIONS_CONFIG"
+        raw_config = File.read(geoserver_config_file)
+        ::VIGILANTE_CONFIG = YAML.load(raw_config)["#{Rails.env}"]
+      # else
+      #   raise GeoserverMigrations::ArgumentError.new("The geoserver_migrations_config.yml is missing. Path=#{geoserver_config_file} Did you run the generator geoserver_migrations:install? ")
+      end
+    end
+
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
+
+  end
+
+
 end
