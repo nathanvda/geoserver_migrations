fa6779601e21c69c10bc9c2283748e7903dc2b52
nathanvda <nathan@dixis.com>
2018-01-06 02:27:35 +0100

    Fix to make it work inside engine
    The `match` works for an outside application but 
    not for the dummy-app inside the engine.

--- lib/geoserver_migrations.rb
+++ lib/geoserver_migrations.rb
@@ -40,11 +40,12 @@ module GeoserverMigrations
     end
 
     initializer :append_migrations do |app|
-      unless app.root.to_s.match root.to_s
+      unless app.root.to_s == root.to_s
         config.paths["db/migrate"].expanded.each do |expanded_path|
           app.config.paths["db/migrate"] << expanded_path
         end
       end
+
     end
 
   end
